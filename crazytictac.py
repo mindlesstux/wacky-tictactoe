a2f88e80c5a79f21b10543a2097155c3fcad96a5
MindlessTux <mindlesstux@users.noreply.github.com>
2022-05-19 23:03:08 -0400

    Update crazytictac.py
    Added row letters

--- crazytictac.py
+++ crazytictac.py
@@ -3,13 +3,16 @@ class game:
     def __init__(self, boardwidth, boardheight):
         self.boardwidth = boardwidth
         self.boardheight = boardheight
-        self.board = [ [ "A" * self.boardwidth ] * self.boardheight ]
+        self.board = [ [ " " * self.boardwidth ] * self.boardheight ]
+        self.rowId = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         self.won = False
 
     def checkWin(self):
         pass
 
     def printBoard(self):
+        print()
+        
         rowstr = ""
         for rows in self.board:
             for idxRow, row in enumerate(rows, start=1):
@@ -17,10 +20,10 @@ class game:
                     pass
                 else:
                     dashes = "-" * ((3*self.boardwidth) + (self.boardwidth - 1))
-                    rowstr = "%s%s\n" % (rowstr, dashes)
+                    rowstr = "%s     %s\n" % (rowstr, dashes)
                 for idxCol in range(len(row)):
                     if idxCol == 0:
-                        rowstr = "%s %s" % (rowstr, row[idxCol])
+                        rowstr = "%s %s    %s" % (rowstr, self.rowId[idxRow - 1], row[idxCol])
                     else:
                         rowstr = "%s | %s" % (rowstr, row[idxCol])
                 rowstr = "%s\n" % (rowstr)
